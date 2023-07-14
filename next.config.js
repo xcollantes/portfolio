81e5f4db67c2bc9d7ca6728a249eec4a3b9dfa1a
Xavier Collantes <collantes.xavier@gmail.com>
2023-07-13 21:00:01 -0700

    Add security headers
    Signed-off-by: Xavier Collantes <collantes.xavier@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,74 @@
+/** NextJS file. */
+
+const contentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  font-src 'self';
+`
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            // HTTPS only
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Server",
+            value: "Apache", // Fake server value
+          },
+          {
+            // Helps against XSS attacks where users try to upload using
+            // different Content-Types
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            // Helps prevent cross-site scripting (XSS), clickjacking, and other
+            // code injection attacks.
+            // Content Security Policy (CSP) can specify allowed origins for
+            // content including scripts, stylesheets, images, fonts, objects,
+            // media (audio, video), iframes, and more.
+            key: "Content-Security-Policy",
+            value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+          },
+          {
+            key: "X-Frame-Options",
+            value: "sameorigin",
+          },
+          {
+            // Stops loading when XSS is detected
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            // Controls how much info is passed to other websites user visits
+            // https://scotthelme.co.uk/a-new-security-header-referrer-policy
+            key: "Referrer-Policy",
+            value: "same-origin",
+          },
+          {
+            // Prevents display in iframes to prevent clickjacking
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+
+          {
+            // Allows which APIs in browser can be accessed
+            // Ex: `camera=(), microphone=(), geolocation=(), browsing-topics=()`
+            key: "Permissions-Policy",
+            value: "geolocation=*", // allow specified policies here
+          },
+        ],
+      },
+    ]
+  },
+}
