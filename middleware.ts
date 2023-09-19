0006b2299582e0424e696b87b53f294aa9060147
Xavier <collantes.xavier@gmail.com>
2023-09-18 19:38:17 -0700

    Have blogs behind login
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- middleware.ts
+++ middleware.ts
@@ -3,4 +3,7 @@
 // Not adding a `config` variable will secure all pages in web application.
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/blogs/(.*)", "/blogs_assets/(.*)"] }
+// Applies to both `pages/` and `public/`.
+export const config = {
+  matcher: ["/blogs/(.*)"],
+}
