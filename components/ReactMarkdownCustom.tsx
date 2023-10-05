ba735d3887ac5a0346e2df855347699ce8bbe150
Xavier <collantes.xavier@gmail.com>
2023-10-04 19:32:29 -0700

    Add blog edits
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- components/ReactMarkdownCustom.tsx
+++ components/ReactMarkdownCustom.tsx
@@ -13,7 +13,7 @@ const imgCustom = (imageData) => {
 
   /** Looking for `![some alt text {500x250}](/the/image/path.png)` => {500x250} */
   // const size = imageData.alt?.match(/\{(\d+)x(\d+)\}/)
-  // const width = size ? size[1] : "400"
+  // const width = size ? size[1] : "100%"
   // const height = size ? size[2] : "250"
 
   /** Looking for `![some alt text {priority}](/the/image/path.png)` => {priority} */
@@ -31,8 +31,8 @@ const imgCustom = (imageData) => {
         alt={altText}
         blurDataURL="https://davisgitonga.dev/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fbanner.749d546a.jpg"
         fill
-        // width={200}
-        // height={500}
+        // width={width}
+        // height={height}
         priority={isPriority}
         placeholder="blur"
         className={imageStyles.imageItem}
