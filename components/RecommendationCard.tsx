0c79bd8d53790dc82af02d60569c97cbb9a396e9
Xavier <collantes.xavier@gmail.com>
2024-01-22 21:02:35 -0800

    Add expand all
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- components/RecommendationCard.tsx
+++ components/RecommendationCard.tsx
@@ -28,41 +28,39 @@ export default function RecommendationCard(props: RecommendationCardType) {
   } = props
 
   return (
-    <Box>
-      <Card raised sx={{ px: 0.5 }}>
-        <CardContent>
-          <Stack spacing={3}>
-            <Stack direction={"row"} alignItems={"center"} spacing={2.5}>
-              <Link href={linkedInLink}>
-                <Avatar
-                  alt="LinkedIn image"
-                  sx={{ width: 50, height: 50 }}
-                  src={profileImagePath}
-                />
-              </Link>
-              <Typography variant="body2">{name}</Typography>
-            </Stack>
-
-            <Typography variant="body1" sx={{ fontWeight: "bold" }}>
-              {headline}
-            </Typography>
-
-            <Typography variant="body1" fontStyle={"italic"}>
-              {previewText}
-            </Typography>
-
-            <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-              <Button
-                variant="outlined"
-                component={MaterialLink}
-                to={`/recs?recId={sio}`}
-              >
-                See full
-              </Button>
-            </Box>
+    <Card raised sx={{ px: 0.5 }}>
+      <CardContent>
+        <Stack spacing={3}>
+          <Stack direction={"row"} alignItems={"center"} spacing={2.5}>
+            <Link href={linkedInLink}>
+              <Avatar
+                alt="LinkedIn image"
+                sx={{ width: 50, height: 50 }}
+                src={profileImagePath}
+              />
+            </Link>
+            <Typography variant="body2">{name}</Typography>
           </Stack>
-        </CardContent>
-      </Card>
-    </Box>
+
+          <Typography variant="body1" sx={{ fontWeight: "bold" }}>
+            {headline}
+          </Typography>
+
+          <Typography variant="body1" fontStyle={"italic"}>
+            {previewText}
+          </Typography>
+
+          <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+            <Button
+              variant="outlined"
+              component={MaterialLink}
+              to={`/recs?recId={sio}`}
+            >
+              See full
+            </Button>
+          </Box>
+        </Stack>
+      </CardContent>
+    </Card>
   )
 }
