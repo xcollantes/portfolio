7147c36cf686a91e0db9344c92183c7389bcea04
Xavier <collantes.xavier@gmail.com>
2025-07-15 18:45:39 -0700

    Fix filter error
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- components/ExperienceCards.tsx
+++ components/ExperienceCards.tsx
@@ -37,7 +37,7 @@ export default function ExperienceCards({ metadata }: ExperienceCardsPropType) {
     } else {
       setSelected(
         metadata.filter((card: MetadataType) =>
-          card.tagIds.some((cardTag: string) => selectedTags.includes(cardTag))
+          card.tagIds?.some((cardTag: string) => selectedTags.includes(cardTag))
         )
       )
     }
