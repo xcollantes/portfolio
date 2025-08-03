34feb90ed30209fd9307c6ee3851ba81763a1802
Xavier <collantes.xavier@gmail.com>
2025-08-02 18:48:43 -0700

    Make darker background
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- components/LongCard.tsx
+++ components/LongCard.tsx
@@ -60,7 +60,7 @@ export default function LongCard({
         left: 0,
         right: 0,
         bottom: 0,
-        backgroundColor: 'rgba(0, 0, 0, 0.48)', // Grayish overlay
+        backgroundColor: 'rgba(0, 0, 0, 0.65)', // Darker overlay
         zIndex: 1,
       }
     }
