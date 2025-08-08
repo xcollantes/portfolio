12f3d7cb81c12ed68b5b99323b063e62799369e5
Xavier <collantes.xavier@gmail.com>
2025-08-08 13:01:56 -0700

    Refactor FilterBar component to use GenericFilterBar
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- components/FilterBar.tsx
+++ components/FilterBar.tsx
@@ -1,39 +1,18 @@
 /** Group of buttons used to filter cards. */
 
-import FilterAltIcon from "@mui/icons-material/FilterAlt"
-import { Box, Stack } from "@mui/material"
-import {
-  FilterDataConfigType,
-  filterDataConfig,
-} from "../article_configs/filters_config"
-import { useToastNotification } from "../hooks/useToastNotification"
-import FilterButton from "./FilterButton"
+import { filterDataConfig } from "../article_configs/filters_config"
+import GenericFilterBar from "./GenericFilterBar"
 
 interface FilterBarPropType {
   disabled?: boolean
 }
 
 export default function FilterBar({ disabled }: FilterBarPropType) {
-  const toast = useToastNotification()
-
-  const handleFilterIconClick = () => {
-    toast.info("Filter by topic")
-  }
-
-  let key: number = 0
   return (
-    <Stack direction={"row"} alignItems={"center"} spacing={1}>
-      <FilterAltIcon color={"disabled"} onClick={handleFilterIconClick} />
-      <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "start" }}>
-        {filterDataConfig.map((filter: FilterDataConfigType) => (
-          <FilterButton
-            displayText={filter.displayText}
-            tagId={filter.tagId}
-            key={key++}
-            disabledBtn={disabled}
-          />
-        ))}
-      </Box>
-    </Stack>
+    <GenericFilterBar
+      filterConfig={filterDataConfig}
+      disabled={disabled}
+      iconTooltip="Filter by topic"
+    />
   )
 }
