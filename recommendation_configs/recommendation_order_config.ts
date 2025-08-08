9a7996deb50885ef44f09243bd13bee01fe392e1
Xavier <collantes.xavier@gmail.com>
2025-08-08 01:10:09 -0700

    Add recommendation order configuration file to specify the display order of markdown recommendations. This file includes an ordered list of recommendation files to be shown on the site.
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

---
+++ recommendation_configs/recommendation_order_config.ts
@@ -0,0 +1,26 @@
+/* cspell:disable */
+
+/** Configuration for the order of recommendations to show. */
+
+/**
+ * Ordered list of recommendation markdown files to include.
+ *
+ * Files are shown in the order specified here.
+ * Only files listed here will be displayed on the site.
+ */
+export const orderedIncludeRecommendationsConfig: string[] = [
+  "siobhan-williams.md",
+  "jeffrey-roth.md",
+  "ehud-persitz.md",
+  "niklas-dorsch.md",
+  "jay-pinnamaneni.md",
+  "ankit-adhikari.md",
+  "francesco-bertolini-pmp-csm.md",
+  "patricia-shirazi.md",
+  "renaud-charrin.md",
+  "edward-petkovicz.md",
+  "philip-hu.md",
+  "rick-collins.md",
+  "brian-mccann.md",
+  "dr-tim-olsen.md",
+];
